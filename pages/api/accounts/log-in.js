a5f656711f5a6e80955a37c8cf02ba39175fe210
Bea <bea.castro@mail.utoronto.ca>
2024-11-01 20:42:06 -0400

    updated implementation for login

--- pages/api/accounts/log-in.js
+++ pages/api/accounts/log-in.js
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { comparePassword, generateToken } from '@/utils/auth';
-const prisma = new PrismaClient()
+import prisma from "../../../utils/db";
+import { comparePassword, generateAccessToken, generateRefreshToken } from "../../../utils/auth";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -10,19 +9,23 @@ export default async function handler(req, res) {
             return res.status(400).json({ message: 'Please fill all required fields.' });
         }
 
+        // find the user through their email
         const user = await prisma.user.findUnique({
             where: {
                 email,
             },
         });
 
+        // compare their password to what's saved in the database
         if (!user || !await comparePassword(password, user.password)) {
-            return res.status(401).json({ message: 'Invalid credentials.' });
+            return res.status(401).json({ message: 'Invalid credentials.', });
         }
 
-        const token = generateToken({ userId: user.id, email: user.email });
+        // generate tokens for the user if the password is correct
+        const accessToken = generateAccessToken({ userId: user.id, email: user.email });
+        const refreshToken = generateRefreshToken({ userId: user.id, email: user.email });
 
-        res.status(200).json( { token });
+        return res.status(200).json( { accessToken, refreshToken });
     } else {
         res.status(405).json({ message: 'Method not allowed.' });
     }
